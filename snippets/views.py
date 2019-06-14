41f9c63b4e0e17b6ef51e42b812426ac04c26746
Ak-shay <mittalakshay2000@gmail.com>
2019-06-14 16:12:32 +0530

    tutorial: 3

--- snippets/views.py
+++ snippets/views.py
@@ -1,51 +1,18 @@
-from rest_framework import status
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
+from rest_framework import mixins
+from rest_framework import generics
 from .models import Snippet
 from .serializers import SnippetSerializer
 
 
-@api_view(['GET', 'POST'])
-def snippet_list(request, format=None):
+class SnippetList(generics.ListCreateAPIView):
 	"""List all code snippets, or create a new snippet"""
 
-	#list of all snippets
-	if request.method == 'GET':
-		snippets = Snippet.objects.all()
-		serializer = SnippetSerializer(snippets, many=True)
-		return Response(serializer.data)
+	queryset = Snippet.objects.all()
+	serializer_class = SnippetSerializer
 
-	#create new snippet
-	elif request.method == 'POST':
-		serializer = SnippetSerializer(data=request.data)
-		if serializer.is_valid():
-			serializer.save()
-			return Response(serializer.data, status=status.HTTP_201_CREATED)
-		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def snippet_detail(request, pk, format=None):
+class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):	
 	"""Retrieve, update or delete a code snippet."""
 	
-	try:
-		snippet = Snippet.objects.get(pk=pk)
-	except Snippet.DoesNotExist:
-		return Response(status=status.HTTP_404_NOT_FOUND)
-
-	#receive snippet
-	if request.method == 'GET':
-		serializer = SnippetSerializer(snippet)
-		return Response(serializer.data)
-
-	#update snippet
-	elif request.method == 'PUT':
-		serializer = SnippetSerializer(snippet, data=request.data)
-		if serializer.is_valid():
-			serializer.save()
-			return Response(serializer.data)
-		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-	#delete snippet
-	elif request.method == 'DELETE':
-		snippet.delete()
-		return Response(status=status.HTTP_204_NO_CONTENT)
+	queryset = Snippet.objects.all()
+	serializer_class = SnippetSerializer
