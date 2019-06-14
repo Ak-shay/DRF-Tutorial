6415832a11e18a99c627d1112fc1b4a97c4da24a
Ak-shay <mittalakshay2000@gmail.com>
2019-06-14 17:41:36 +0530

    tutorial: 4

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -1,11 +1,21 @@
 from rest_framework import serializers
 from .models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
+from django.contrib.auth.models import User
 
+
+class UserSerializer(serializers.ModelSerializer):
+	snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
+
+	class Meta:
+		model = User
+		fields = ('id', 'username', 'snippets')
 
 class SnippetSerializer(serializers.ModelSerializer):
 	"""Class defines the fields that get serialized/deserialized"""
 	#serializer.ModelSerializer is simple implementation for create() and update() 
 	
+	owner = serializers.ReadOnlyField(source='owner.username')
+
 	class Meta:
 		model = Snippet
-		fields = ('id', 'title', 'code', 'linenos', 'language', 'style')
+		fields = ('id', 'owner', 'title', 'code', 'linenos', 'language', 'style')
