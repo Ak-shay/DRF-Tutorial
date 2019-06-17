99d48a76b06b741d3c5f7c860f3861ec12238432
Ak-shay <mittalakshay2000@gmail.com>
2019-06-18 00:54:35 +0530

    tutorial: 7

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from rest_framework.schemas import get_schema_view
 from . import views
 
 
@@ -8,7 +9,10 @@ router = DefaultRouter()
 router.register(r'snippets', views.SnippetViewSet)
 router.register(r'users', views.UserViewSet)
 
+schema_view = get_schema_view(title='Pastebin API')
+
 #The API URLS are now determined automatically by the router.
 urlpatterns = [
 	path('', include(router.urls)),
+	path('schema/', schema_view),
 ]
